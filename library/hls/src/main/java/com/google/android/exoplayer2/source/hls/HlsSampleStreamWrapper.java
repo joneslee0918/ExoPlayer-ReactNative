google/ExoPlayer

commit 4783c329cc545da4269a9401a4975a41e75f2f43

Map HLS sample formats to the correct codec string
This change fixes format creation for traditional preparation of streams
where the master playlist contains more than one codec string per track
type.

Issue: #7877
PiperOrigin-RevId: 338538693

library/hls/src/main/java/com/google/android/exoplayer2/source/hls/HlsSampleStreamWrapper.java
@@ -1404,8 +1404,10 @@ import org.checkerframework.checker.nullness.qual.RequiresNonNull;
       return sampleFormat;
     }
 
-    int sampleTrackType = MimeTypes.getTrackType(sampleFormat.sampleMimeType);
-    @Nullable String codecs = Util.getCodecsOfType(playlistFormat.codecs, sampleTrackType);
+    @Nullable
+    String codecs =
+        MimeTypes.getCodecsCorrespondingToMimeType(
+            playlistFormat.codecs, sampleFormat.sampleMimeType);
     @Nullable String sampleMimeType = MimeTypes.getMediaMimeType(codecs);
 
     Format.Builder formatBuilder =

library/common/src/main/java/com/google/android/exoplayer2/util/MimeTypes.java
@@ -250,16 +250,37 @@ public final class MimeTypes {
    */
   public static boolean containsCodecsCorrespondingToMimeType(
       @Nullable String codecs, String mimeType) {
-    if (codecs == null) {
-      return false;
+    return getCodecsCorrespondingToMimeType(codecs, mimeType) != null;
+  }
+
+  /**
+   * Returns a subsequence of {@code codecs} containing the codec strings that correspond to the
+   * given {@code mimeType}. Returns null if {@code mimeType} is null, {@code codecs} is null, or
+   * {@code codecs} does not contain a codec that corresponds to {@code mimeType}.
+   *
+   * @param codecs An RFC 6381 codecs string.
+   * @param mimeType A MIME type to look for.
+   * @return A subsequence of {@code codecs} containing the codec strings that correspond to the
+   *     given {@code mimeType}. Returns null if {@code mimeType} is null, {@code codecs} is null,
+   *     or {@code codecs} does not contain a codec that corresponds to {@code mimeType}.
+   */
+  @Nullable
+  public static String getCodecsCorrespondingToMimeType(
+      @Nullable String codecs, @Nullable String mimeType) {
+    if (codecs == null || mimeType == null) {
+      return null;
     }
     String[] codecList = Util.splitCodecs(codecs);
+    StringBuilder builder = new StringBuilder();
     for (String codec : codecList) {
       if (mimeType.equals(getMediaMimeType(codec))) {
-        return true;
+        if (builder.length() > 0) {
+          builder.append(",");
+        }
+        builder.append(codec);
       }
     }
-    return false;
+    return builder.length() > 0 ? builder.toString() : null;
   }
 
   /**

library/common/src/test/java/com/google/android/exoplayer2/util/MimeTypesTest.java
@@ -61,6 +61,37 @@ public final class MimeTypesTest {
   }
 
   @Test
+  public void getCodecsCorrespondingToMimeType_returnsCorrectResult() {
+    assertThat(
+            MimeTypes.getCodecsCorrespondingToMimeType(
+                /* codecs= */ "avc1.4D5015,ac-3,mp4a.40.2,avc1.4D4015", MimeTypes.AUDIO_AAC))
+        .isEqualTo("mp4a.40.2");
+    assertThat(
+            MimeTypes.getCodecsCorrespondingToMimeType(
+                /* codecs= */ "avc1.4D5015,ac-3,mp4a.40.2,avc1.4D4015", MimeTypes.VIDEO_H264))
+        .isEqualTo("avc1.4D5015,avc1.4D4015");
+    assertThat(
+            MimeTypes.getCodecsCorrespondingToMimeType(
+                /* codecs= */ "avc1.4D5015,ac-3,mp4a.40.2,avc1.4D4015", MimeTypes.AUDIO_AC3))
+        .isEqualTo("ac-3");
+    assertThat(
+            MimeTypes.getCodecsCorrespondingToMimeType(
+                /* codecs= */ "unknown-codec,ac-3,mp4a.40.2,avc1.4D4015", MimeTypes.AUDIO_AC3))
+        .isEqualTo("ac-3");
+
+    assertThat(
+            MimeTypes.getCodecsCorrespondingToMimeType(
+                /* codecs= */ "avc1.4D5015,ac-3,mp4a.40.2,avc1.4D4015", MimeTypes.VIDEO_H265))
+        .isNull();
+    assertThat(
+            MimeTypes.getCodecsCorrespondingToMimeType(
+                /* codecs= */ "avc1.4D5015,ac-3,mp4a.40.2,avc1.4D4015", null))
+        .isNull();
+    assertThat(MimeTypes.getCodecsCorrespondingToMimeType(/* codecs= */ null, MimeTypes.AUDIO_AAC))
+        .isNull();
+  }
+
+  @Test
   public void isText_returnsCorrectResult() {
     assertThat(MimeTypes.isText(MimeTypes.TEXT_VTT)).isTrue();
     assertThat(MimeTypes.isText(MimeTypes.TEXT_SSA)).isTrue();

joneslee0918/ExoPlayer-ReactNative

commit 2b9e9961b27140ddd0613259e3518f70bc817cdb

Fix HLS format derivation in traditional preparation
ExoPlayer's traditional HLS preparation works by loading a chunk from each track
group, and then tries to use the sample information plus the master playlist
information to generate the preparation's resulting TrackGroups.

There are 3 possible scenarios:
- Supported case: Each variant has a single codec string per track type. We can
  assign each track the codec string which matches the loaded sample's type.
- Supported case: Each variant has more than one codec string, but each track
  group has a single track. This is the case when different languages use
  different codecs. In this case, we can assign whichever codec matches the
  loaded sample's mime type.
- Unsupported case: Each variant has more than one codec string, and track
  groups contain more than one track. We are not able to safely map tracks to
  codec strings because that would require loading a chunk from each track
  (which would considerably delay preparation).

Broken in:
https://github.com/google/ExoPlayer/commit/4783c329cc545da4269a9401a4975a41e75f2f43

PiperOrigin-RevId: 343072201

library/hls/src/main/java/com/google/android/exoplayer2/source/hls/HlsSampleStreamWrapper.java
@@ -1396,7 +1396,8 @@ import org.checkerframework.checker.nullness.qual.RequiresNonNull;
 
   /**
    * Derives a track sample format from the corresponding format in the master playlist, and a
-   * sample format that may have been obtained from a chunk belonging to a different track.
+   * sample format that may have been obtained from a chunk belonging to a different track in the
+   * same track group.
    *
    * @param playlistFormat The format information obtained from the master playlist.
    * @param sampleFormat The format information obtained from the samples.
@@ -1410,11 +1411,23 @@ import org.checkerframework.checker.nullness.qual.RequiresNonNull;
       return sampleFormat;
     }
 
-    @Nullable
-    String codecs =
-        MimeTypes.getCodecsCorrespondingToMimeType(
-            playlistFormat.codecs, sampleFormat.sampleMimeType);
-    @Nullable String sampleMimeType = MimeTypes.getMediaMimeType(codecs);
+    int sampleTrackType = MimeTypes.getTrackType(sampleFormat.sampleMimeType);
+    @Nullable String sampleMimeType;
+    @Nullable String codecs;
+    if (Util.getCodecCountOfType(playlistFormat.codecs, sampleTrackType) == 1) {
+      // We can unequivocally map this track to a playlist variant because only one codec string
+      // matches this track's type.
+      codecs = Util.getCodecsOfType(playlistFormat.codecs, sampleTrackType);
+      sampleMimeType = MimeTypes.getMediaMimeType(codecs);
+    } else {
+      // The variant assigns more than one codec string to this track. We choose whichever codec
+      // string matches the sample mime type. This can happen when different languages are encoded
+      // using different codecs.
+      codecs =
+          MimeTypes.getCodecsCorrespondingToMimeType(
+              playlistFormat.codecs, sampleFormat.sampleMimeType);
+      sampleMimeType = sampleFormat.sampleMimeType;
+    }
 
     Format.Builder formatBuilder =
         sampleFormat