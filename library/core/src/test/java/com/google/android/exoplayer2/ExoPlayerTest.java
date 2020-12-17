google/ExoPlayer

commit b1e9257de143e9f377895b63868ef47b4fc0eea9

Fix the type of message passed with MSG_PLAYBACK_INFO_CHANGED
The receiving code was updated in:
https://github.com/google/ExoPlayer/commit/7b82a3c8892a2b90fa1121dc6bfad7817ae03944
but this sending site was missed.

PiperOrigin-RevId: 320173033

library/core/src/main/java/com/google/android/exoplayer2/ExoPlayerImpl.java
@@ -591,9 +591,7 @@ import java.util.concurrent.TimeoutException;
       applicationHandler
           .obtainMessage(
               ExoPlayerImplInternal.MSG_PLAYBACK_INFO_CHANGED,
-              /* operationAcks */ 1,
-              /* positionDiscontinuityReason */ C.INDEX_UNSET,
-              playbackInfo)
+              new ExoPlayerImplInternal.PlaybackInfoUpdate(playbackInfo))
           .sendToTarget();
       return;
     }

joneslee0918/ExoPlayer-ReactNative

commit 707d3a3ff0a45ce7f29167819b871b5e39bb5f58

Fix EPI.seekTo to balance operation acks when seeking during an ad
This regression was introduced in
https://github.com/google/ExoPlayer/commit/b1e9257de143e9f377895b63868ef47b4fc0eea9

Issue: #8349

#minor-release

PiperOrigin-RevId: 347802049

library/core/src/main/java/com/google/android/exoplayer2/ExoPlayerImpl.java
@@ -613,8 +613,10 @@ import java.util.concurrent.TimeoutException;
       // general because the midroll ad preceding the seek destination must be played before the
       // content position can be played, if a different ad is playing at the moment.
       Log.w(TAG, "seekTo ignored because an ad is playing");
-      playbackInfoUpdateListener.onPlaybackInfoUpdate(
-          new ExoPlayerImplInternal.PlaybackInfoUpdate(playbackInfo));
+      ExoPlayerImplInternal.PlaybackInfoUpdate playbackInfoUpdate =
+          new ExoPlayerImplInternal.PlaybackInfoUpdate(this.playbackInfo);
+      playbackInfoUpdate.incrementPendingOperationAcks(1);
+      playbackInfoUpdateListener.onPlaybackInfoUpdate(playbackInfoUpdate);
       return;
     }
     @Player.State

library/core/src/test/java/com/google/android/exoplayer2/ExoPlayerTest.java
@@ -4490,6 +4490,42 @@ public final class ExoPlayerTest {
     assertThat(totalBufferedDurationMs[1]).isEqualTo(adDurationMs);
   }
 
+  // https://github.com/google/ExoPlayer/issues/8349
+  @Test
+  public void seekTo_whilePlayingAd_doesntBlockFutureUpdates() throws Exception {
+    long contentDurationMs = 10_000;
+    long adDurationMs = 4_000;
+    AdPlaybackState adPlaybackState =
+        new AdPlaybackState(/* adsId= */ new Object(), /* adGroupTimesUs...= */ 0)
+            .withAdCount(/* adGroupIndex= */ 0, /* adCount= */ 1)
+            .withAdUri(/* adGroupIndex= */ 0, /* adIndexInAdGroup= */ 0, Uri.EMPTY);
+    long[][] durationsUs = new long[1][];
+    durationsUs[0] = new long[] {C.msToUs(adDurationMs)};
+    adPlaybackState = adPlaybackState.withAdDurationsUs(durationsUs);
+    Timeline adTimeline =
+        new FakeTimeline(
+            new TimelineWindowDefinition(
+                /* periodCount= */ 1,
+                /* id= */ 0,
+                /* isSeekable= */ true,
+                /* isDynamic= */ false,
+                /* durationUs= */ C.msToUs(contentDurationMs),
+                adPlaybackState));
+    FakeMediaSource adsMediaSource = new FakeMediaSource(adTimeline);
+
+    SimpleExoPlayer player = new TestExoPlayerBuilder(context).build();
+    player.setMediaSource(adsMediaSource);
+    player.pause();
+    player.prepare();
+    runUntilPlaybackState(player, Player.STATE_READY);
+
+    player.seekTo(0, 8000);
+    player.play();
+
+    // This times out if playback info updates after the seek are blocked.
+    runUntilPlaybackState(player, Player.STATE_ENDED);
+  }
+
   @Test
   public void becomingNoisyIgnoredIfBecomingNoisyHandlingIsDisabled() throws Exception {
     CountDownLatch becomingNoisyHandlingDisabled = new CountDownLatch(1);